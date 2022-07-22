9ad11cf830cb1cd4701863555c52bebb30828509
chenyuhui <2236212007@qq.com>
2022-07-22 10:07:31 +0800

    hot-fix second commit

--- src/main/java/com/cyh/GitTest.java
+++ src/main/java/com/cyh/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("Git Demo 222");
         System.out.println("Git Demo 333");
         System.out.println("hot-fix commit 1");
+        System.out.println("hot-fix commit 2");
     }
 }
