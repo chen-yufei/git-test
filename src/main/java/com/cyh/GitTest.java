5b18ea2f405ba765459dffdc5ef4023f13fb4d0a
chenyuhui <2236212007@qq.com>
2022-07-22 09:39:14 +0800

    first commit

--- src/main/java/com/cyh/GitTest.java
+++ src/main/java/com/cyh/GitTest.java
@@ -3,5 +3,6 @@ package com.cyh;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Git Demo 111");
+        System.out.println("Git Demo 222");
     }
 }
