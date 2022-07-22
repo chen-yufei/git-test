e041a359348d679c3d582e2f27b6b96f20c8c1eb
chen-yufei <57556503+chen-yufei@users.noreply.github.com>
2022-07-22 10:41:35 +0800

    master pull test

--- src/main/java/com/cyh/GitTest.java
+++ src/main/java/com/cyh/GitTest.java
@@ -9,5 +9,7 @@ public class GitTest {
         System.out.println("hot-fix commit 2");
         System.out.println("master commit 1");
         System.out.println("master push test");
+        System.out.println("master pull test");
+        
     }
 }
